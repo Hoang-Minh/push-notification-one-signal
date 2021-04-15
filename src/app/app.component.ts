f7b8a4be312b612903d3b73c73310d585c680ef8
minh@partie.com <minh@partie.com>
2021-04-15 13:11:14 -0700

    fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,66 +12,36 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
 
     console.log('1. Init OneSignal');
-    const OneSignal = window['OneSignal'] || [];
-    OneSignal.SERVICE_WORKER_PARAM = { scope: '/assets/js/' };
-    OneSignal.SERVICE_WORKER_PATH = '../assets/js/OneSignalSDKWorker.js';
-    OneSignal.SERVICE_WORKER_UPDATER_PATH = '../assets/js/OneSignalSDKUpdaterWorker.js';
+    
+    let oneSignal = window["OneSignal"] || [];
 
-    const oneSignalInit = {
-      appId: environment.oneSignalAppId,
+    oneSignal.SERVICE_WORKER_PARAM = { scope: '/assets/js/one-signal/' };
+    oneSignal.SERVICE_WORKER_PATH = '../assets/js/one-signal/OneSignalSDKWorker.js';
+    oneSignal.SERVICE_WORKER_UPDATER_PATH = '../assets/js/one-signal/OneSignalSDKUpdaterWorker.js';
+    const initConfig = {
+      appId: environment.oneSignalSafariId,
       safari_web_id: environment.oneSignalSafariId,
       autoResubscribe: true,
       allowLocalhostAsSecureOrigin: true,
       promptOptions: {
         slidedown: {
-          enabled: true,
-          autoPrompt: true,
+          enabled: true,              
           timeDelay: 5,
           //pageViews: 3,
-          actionMessage: "We'd like to show you notifications for the latest news and updates.",
+          actionMessage: "We'd like to show you notifications for the latest news and updates from Partie.",
           acceptButtonText: "ALLOW",
-          cancelButtonText: "NO THANKS",
-        }
+          cancelButtonText: "NO THANKS"              
+        },            
       },
       notifyButton: {
         enable: true
       },
-      persistNotification: false
-    }        
+      persistNotification: false          
+    };        
 
-    OneSignal.push(['init', oneSignalInit]);
-    
+    oneSignal.push(function() {          
 
-    console.log('2. OneSignal Initialized', OneSignal);
-
-    OneSignal.push(() => {
-      console.log('3. Register For Push');
-
-      OneSignal.push(['registerForPushNotifications']);
-
-      OneSignal.getUserId().then((userId) => {
-          console.log('4. User ID is', userId);
-        });             
-    });
-
-    OneSignal.push(() => {
-
-      OneSignal.on('subscriptionChange', (isSubscribed) => {
-        console.log('The users subscription state is now:', isSubscribed);
-
-        if(isSubscribed) {
-
-          console.log(`The users subscription state is now: ${isSubscribed}. Trying to set externalId`);
-          OneSignal.setExternalUserId("E026FC3C-2E06-46F6-A8D1-ABF5CC98E9EC");
-        }
-
-        // OneSignal.getUserId().then((userId) => {
-        //   console.log('5. User ID is from subscription change', userId);
-        // });
-      });
-
-      console.log('6. OneSignal Initialized', OneSignal);
-
+      oneSignal.init(initConfig);
     });
     
   }
